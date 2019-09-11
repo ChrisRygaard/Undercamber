fa31ebecb1d02f6d0aa334d12a76f804b0e2af22
Chris Rygaard <chris.rygaard@alpinefir.com>
2019-09-11 09:36:24 -0500

    The file headers in OperatingSystem.java and RequirementsWindow.java were incorrect: - The names "AlpineFir" and "Alpine Fir" were DBAs of Rygaard Technologies, LLC and are defunct. - The license information in OperatingSystem.java was always intended to match the overall license of the Undercamber project.  Its header was incorrect due to human error. Update these files to include the correct, intended headers.

--- Source/com/undercamber/RequirementsWindow.java
+++ Source/com/undercamber/RequirementsWindow.java
@@ -1,4 +1,4 @@
-// Copyright 2018 AlpinePeak Systems, LLC
+// Copyright 2018 Rygaard Technologies, LLC
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted
 // provided that the following conditions are met:
